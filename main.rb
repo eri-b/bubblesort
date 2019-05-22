e10e03e7b1c4ee1b0bb0a4787aed67dd7774059a
DevMFernandes <contato@marcos-fernandes.com>
2019-05-22 17:09:16 -0300

    first steps

--- main.rb
+++ main.rb
@@ -1,1 +1,14 @@
-puts "hello"
+def bubble_sort(arr)
+    arr.each_with_index do |x, index|
+        break if index >= arr.length-1
+        if x > arr[index.next]
+            puts "larger"
+        elsif x < arr[index.next]
+            puts "not larger"
+        else
+            
+        end
+    end
+end
+
+bubble_sort([1, 2, 3])
